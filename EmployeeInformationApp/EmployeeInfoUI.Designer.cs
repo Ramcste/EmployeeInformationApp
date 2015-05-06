82d49b7493b9c7e133f9806b0eb8596e3d802015
ramcste <lucky.guyram@gmail.com>
2015-05-06 13:05:17 +0600

    Initial Commit

---
+++ EmployeeInformationApp/EmployeeInfoUI.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace EmployeeInformationApp
+{
+    partial class EmployeeInfoUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.employeeListView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(177, 64);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(206, 20);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(177, 117);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(206, 20);
+            this.addressTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(108, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Address";
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(177, 164);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(206, 20);
+            this.emailTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(108, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Email";
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Location = new System.Drawing.Point(177, 210);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.Size = new System.Drawing.Size(206, 20);
+            this.salaryTextBox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(108, 213);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Salary";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(308, 264);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 8;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // employeeListView
+            // 
+            this.employeeListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.employeeListView.Location = new System.Drawing.Point(80, 342);
+            this.employeeListView.Name = "employeeListView";
+            this.employeeListView.Size = new System.Drawing.Size(344, 207);
+            this.employeeListView.TabIndex = 9;
+            this.employeeListView.UseCompatibleStateImageBehavior = false;
+            this.employeeListView.View = System.Windows.Forms.View.Details;
+            this.employeeListView.SelectedIndexChanged += new System.EventHandler(this.employeeListView_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Address";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Email";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Salary";
+            // 
+            // EmployeeInfoUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 561);
+            this.Controls.Add(this.employeeListView);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.salaryTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "EmployeeInfoUI";
+            this.Text = "Employee Info App";
+            this.Load += new System.EventHandler(this.EmployeeInfoUI_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox salaryTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.ListView employeeListView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+    }
+}
+
---
+++ EmployeeInformationApp/EmployeeInfoUI.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeInformationApp
+{
+    public partial class EmployeeInfoUI : Form
+    {
+        public EmployeeInfoUI()
+        {
+            InitializeComponent();
+        }
+
+        private bool isemailexits = false;
+
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            Employee emp = new Employee();
+
+            emp.name = nameTextBox.Text;
+            emp.address = addressTextBox.Text;
+            emp.email = emailTextBox.Text;
+            emp.salary = float.Parse(salaryTextBox.Text);
+
+            isemailexits = IsEmailExits(emp.email);
+
+
+            if (isemailexits)
+            {
+                MessageBox.Show("This email id already exists");
+            }
+
+            else
+            {
+
+                string connectionString = @"Server=.\SQLEXPRESS;Database=Company;Integrated Security=true;";
+
+                SqlConnection connection = new SqlConnection(connectionString);
+
+                string query = "INSERT INTO Employees VALUES('" + emp.name + "','" + emp.address + "','" + emp.email +
+                               "','" + emp.salary + "')";
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                connection.Open();
+                int rowseffected = command.ExecuteNonQuery();
+                connection.Close();
+
+                if (rowseffected > 0)
+                {
+                    MessageBox.Show("Information is saved");
+                    GetClearTextBoxes();
+                    GetDataLoadedInListView();
+
+                }
+                else
+                {
+                    MessageBox.Show("Error!!");
+                }
+
+            }
+        }
+
+        private void GetClearTextBoxes()
+        {
+            nameTextBox.Text = "";
+            addressTextBox.Text = "";
+            emailTextBox.Text = "";
+            salaryTextBox.Text = "";
+        }
+
+        private void EmployeeInfoUI_Load(object sender, EventArgs e)
+        {
+            GetDataLoadedInListView();
+
+        }
+
+        private bool IsEmailExits(string email)
+        {
+
+
+            string connectionString = @"Server=.\SQLEXPRESS;Database=Company;Integrated Security=true;";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            string query = "SELECT e_email FROM Employees WHERE e_email='" + email + "'";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                isemailexits = true;
+            }
+            connection.Close();
+
+            return isemailexits;
+
+        }
+
+
+
+        private void GetDataLoadedInListView()
+        {
+            employeeListView.Items.Clear();
+
+            List<Employee> employees = new List<Employee>();
+
+            string connectionString = @"Server=.\SQLEXPRESS;Database=Company;Integrated Security=true;";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            string query = "SELECT * FROM Employees";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Employee anEmployee = new Employee();
+
+                anEmployee.id = int.Parse(reader["e_id"].ToString());
+                anEmployee.name = reader["e_name"].ToString();
+                anEmployee.address = reader["e_address"].ToString();
+                anEmployee.email = reader["e_email"].ToString();
+                anEmployee.salary = float.Parse(reader["e_salary"].ToString());
+
+                employees.Add(anEmployee);
+
+            }
+            connection.Close();
+
+            foreach (var employee in employees)
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = employee.id.ToString();
+                item.SubItems.Add(employee.name);
+                item.SubItems.Add(employee.address);
+                item.SubItems.Add(employee.email);
+                item.SubItems.Add(employee.salary.ToString());
+                employeeListView.Items.Add(item);
+            }
+
+
+
+
+
+        }
+
+        private void employeeListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int id = int.Parse(employeeListView.SelectedItems[0].Text);
+
+            string connectionString = @"Server=.\SQLEXPRESS;Database=Company;Integrated Security=true;";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            string query = "SELECT e_name,e_address,e_email,e_salary FROM Employees WHERE e_id='"+id+"'";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                
+                nameTextBox.Text = reader["e_name"].ToString();
+                addressTextBox.Text = reader["e_address"].ToString();
+                emailTextBox.Text = reader["e_email"].ToString();
+                salaryTextBox.Text = reader["e_salary"].ToString();
+
+
+            }
+
+            connection.Close();
+        }
+    }
+}
